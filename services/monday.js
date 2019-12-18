06bc38db04447b8320d980c19b9143d40e1bd573
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-12-18 18:22:35 +0200

    hope it help

--- services/monday.js
+++ services/monday.js
@@ -176,7 +176,7 @@ const getUserUnfinishedTasksToday = async (userId) => {
 const assignItem = (boardId, itemId, userId) => {
   const query = `
     mutation {
-      change_column_value (board_id: ${boardId}, item_id: ${itemId}, column_id: "person", value: ${JSON.stringify({ id: userId })}) {
+      change_column_value (board_id: ${boardId}, item_id: ${itemId}, column_id: "person", value: ${JSON.stringify({ "id": userId })}) {
         id
       }
     }
